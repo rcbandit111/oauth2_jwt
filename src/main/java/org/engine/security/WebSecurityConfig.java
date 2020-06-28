71e20e7019b48e4aa88528ece0334b6804f79540
plte1219 <plamen.terziev@netcracker.com>
2020-06-28 22:04:53 +0300

    Updated code

--- src/main/java/org/engine/security/WebSecurityConfig.java
+++ src/main/java/org/engine/security/WebSecurityConfig.java
@@ -25,6 +25,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // @formatter:off
+        http.authorizeRequests()
+                .antMatchers("/users/authorize").permitAll()
+                .antMatchers("/users/reset_request").permitAll()
+                .antMatchers("/users/reset_token").permitAll()
+                .antMatchers("/users/reset_password").permitAll()
+                .antMatchers("/users/confirmation_token").permitAll()
+                .antMatchers("/users/reset_user_password").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().csrf().disable();
+
+        // No session will be created or used by spring security
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // @formatter:on
     }
 
     @Override
